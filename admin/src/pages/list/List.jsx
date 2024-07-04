7e8dcf9365ea5d26aa75ad6b660f3d641b8762ea
Ayush Srivastava <ayush.skt001@gmail.com>
2024-07-04 13:33:16 +0530

    all files added

---
+++ admin/src/pages/list/List.jsx
@@ -0,0 +1,20 @@
+import "./list.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
+import React from "react"
+
+const List = ({columns}) => {
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable columns={columns}/>
+        {console.log(columns)};
+      </div>
+    </div>
+  )
+}
+
+export default List
